55ce9fb9ccae13e1d2df9db69d6e9adb238fb466
MJackson22-bit <jm5444859@gmail.com>
2022-06-23 00:17:04 -0600

    Implemented bottom navigation
    I was implemented the bottom navigation for move between the fragments

--- app/src/main/java/com/example/finalproject/view/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/view/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.finalproject.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.finalproject.R
+import com.example.finalproject.databinding.ActivityAddBinding
+import com.example.finalproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
